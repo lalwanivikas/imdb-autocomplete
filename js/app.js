2fc3a3ea517960a0120b166ea1a6b2da4505168e
Vikas Lalwani <lalwani.vikas20@gmail.com>
2015-10-16 20:39:50 +0530

    initial commit

---
+++ js/app.js
@@ -0,0 +1,85 @@
+var $input = document.getElementById('searchBox');
+var baseUrl = "http://sg.media-imdb.com/suggests/";
+var $result = document.getElementById('result');
+var body = document.getElementsByTagName('body');
+
+$input.addEventListener('keyup', function(){
+
+	//clearing blank spaces from input
+	var cleanInput = $input.value.replace(/\s/g, "");
+	
+	//clearing result div if the input box in empty
+	if(cleanInput.length === 0) {
+		$result.innerHTML = "";
+	}
+	
+	if(cleanInput.length > 0) {
+		$result.innerHTML = "";
+		
+		var queryUrl = baseUrl + cleanInput[0].toLowerCase() + "/" 
+					  + cleanInput.toLowerCase()
+					  + ".json";	
+		$.ajax({
+		    
+		    url: queryUrl,
+		    dataType: 'jsonp',
+		    cache: true,
+		    jsonp: false,
+		    jsonpCallback: "imdb$" + cleanInput.toLowerCase()
+		
+		}).done(function (result) {
+	    
+		    for(var i = 0; i < result.d.length; i++) {
+		    	
+		    	var category = result.d[i].id.slice(0,2);
+		    	
+		    	if(category === "tt" || category === "nm") {		    		
+		    		//row for risplaying one result
+		    		var resultRow = document.createElement('a');
+		    		resultRow.setAttribute('class', 'resultRow');
+		    		var destinationUrl;
+
+		    		if(category === "tt") {
+		    			destinationUrl = "http://www.imdb.com/title/" + result.d[i].id;
+		    		} else {
+		    			destinationUrl = "http://www.imdb.com/name/" + result.d[i].id;
+		    		}
+		    		
+		    		resultRow.setAttribute('href', destinationUrl);
+		    		resultRow.setAttribute('target', '_blank');
+		    		
+
+		    		//creating and setting poster
+		    		var poster = document.createElement('img');
+		    		poster.setAttribute('class', 'poster');
+		    		
+		    		if(result.d[i].i) {
+			    		var posterUrl = result.d[i].i[0];
+			    		posterUrl = posterUrl.replace("._V1_.jpg", "._V1._SX40_CR0,0,40,54_.jpg");
+			    		poster.setAttribute('src', posterUrl);
+		    		}
+		    		//creating and setting description
+		    		var description = document.createElement('div');
+		    		description.setAttribute('class', 'description');
+		    		var name = document.createElement('h4');
+		    		var snippet = document.createElement('h5');
+
+		    		if(category === "tt" && result.d[i].y) {
+		    			name.innerHTML = result.d[i].l + " (" + result.d[i].y + ")";
+		    		} else {
+		    			name.innerHTML = result.d[i].l;
+		    		}
+		    		snippet.innerHTML = result.d[i].s;
+
+		    		$(description).append(name);
+		    		$(description).append(snippet);
+
+		    		$(resultRow).append(poster);
+		    		$(resultRow).append(description);
+		    		$("#result").append(resultRow);
+		    	}
+		    }
+		
+		});
+	}
+});
